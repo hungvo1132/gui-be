74feea5345e4ff7620068a0c3dc32c5b48bdc666
Hung <karikahami@gmail.com>
2021-10-13 10:23:19 +0700

    init

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'helu bé!',
+    introDesc: `Tớ có điều này muốn hỏi cậu á, cậu nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'cậu có yêu tớ hong ... ',
+    desc: 'cậu phải trả lời đó nghen :< , đừng thoát ra đó ',
+    btnYes: 'Có,  tớ cũng thích cậu 💖💖💖',
+    btnNo: '💔 Cậu mơ à ! 💔 ',
+    question:' Vì sao cậu thích tớ z :>',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'tớ không biết nữa',
+    mess: 'hì , tớ biết cậu sẽ nhắn v mà , nhưng hong sao đâu , tớ sẽ khiến cậu có lí do để yêu tớ đến hết đời :>',
+    messDesc: 'z nên là bỏ qua cho tớ lần này nghen ',
+    btnAccept: 'tạm cho qua đó',
+    messLink: 'https://m.me/profile.php?id=100066201860391'
+}
